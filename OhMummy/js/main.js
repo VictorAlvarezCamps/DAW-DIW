e3950de0c105d0e612042e4abf35b6fbfd26f1e9
Victor Alvarez <victor.alvarez.camps@gmail.com>
2019-10-16 19:11:57 +0200

    Mapa creado

--- OhMummy/js/main.js
+++ OhMummy/js/main.js
@@ -1,13 +1,21 @@
 /*Importar clases*/
-import Mapa from '../js/Mapa.js';
+import Mapa from './Mapa.js';
+import Aventurero from './Aventurero.js';
+import Llave from './Llave.js';
+import Momia from './Momia.js';
+import Pergamino from './Pergamino.js';
+import Urna from './Urna.js';
 
 /*Variables*/
+let m;
+
 
 window.onload = function(){
 
-    let m = new Mapa();
+    m = new Mapa();
 
     m.CrearMapa();
 
 };
+
 
