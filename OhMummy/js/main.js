4810d67659adb6319463a989a8fdf9e1a46b97d8
Victor Alvarez <victor.alvarez.camps@gmail.com>
2019-10-18 01:18:57 +0200

    Metiendo personajes

--- OhMummy/js/main.js
+++ OhMummy/js/main.js
@@ -1,42 +1,57 @@
-/*Importar clases*/
-import Mapa from './Mapa.js';
-import Aventurero from './Aventurero.js';
-import Llave from './Llave.js';
-import Momia from './Momia.js';
-import Pergamino from './Pergamino.js';
-import Urna from './Urna.js';
-import Barra from './Barra.js';
-
 /*Variables*/
-let m;
-let b;
-// var mapa = [[1,1,1,1,1,1,1,1,1,2,2,2,1,1,1,1,1,1,1,1,1],
-//            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-//            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-//            [0,0,0,1,1,1,0,0,0,1,1,1,0,0,0,1,1,1,0,0,0],
-//            [0,0,0,1,1,1,0,0,0,1,1,1,0,0,0,1,1,1,0,0,0],
-//            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-//            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-//            [0,0,0,1,1,1,0,0,0,1,1,1,0,0,0,1,1,1,0,0,0],
-//            [0,0,0,1,1,1,0,0,0,1,1,1,0,0,0,1,1,1,0,0,0],
-//            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-//            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-//            [0,0,0,1,1,1,0,0,0,1,1,1,0,0,0,1,1,1,0,0,0],
-//            [0,0,0,1,1,1,0,0,0,1,1,1,0,0,0,1,1,1,0,0,0],
-//            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-//            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]];
+var mapa = document.getElementById("mapa");
 
-
-var mapa2 = [[2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2],
+var mapa2 = [[2,2,2,2,2,2,2,2,2,2,2,0,2,2,2,2,2,2,2,2,2,2,2],
             [2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2],
-            [2,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,2]];
+            [2,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,2],
+            [2,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,2],
+            [2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2],
+            [2,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,2],
+            [2,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,2],
+            [2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2],
+            [2,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,2],
+            [2,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,2],
+            [2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2],
+            [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2],
+        ];
 
-
+   
 
 window.onload = function(){
 
-    var mapa = document.getElementById("mapa");
+    CrearMapa();
 
+    BarraObjetos();
+
+    PJ();
+};
+
+/*Detectar movimiento personaje*/
+
+document.addEventListener('keydown', (event) => {
+    const keyName = event.key;
+    console.log('keydown event\n\n' + 'key: ' + keyName);
+
+    switch(event.key){
+        case "a":
+
+        break;
+        case "d":
+
+        break;
+        case "w":
+
+        break;
+        case "s":
+
+        break;
+    }
+
+  });
+
+/*Crear mapa*/
+
+function CrearMapa(){
     for(let i = 0; i < mapa2.length ; i++) {
         for (let j = 0; j < mapa2[i].length ; j++) {
             var div = document.createElement("div");
@@ -47,55 +62,48 @@ window.onload = function(){
             mapa2[i][j] = div;
         }
     }
-
-    // m = new Mapa();
-
-    // m.CrearMapa(mapa);
-
-    // b = new Barra();
-
-    // b.CrearBarra();
-
-    /*for(let i=0;i<20;i++){
-
-
-    }*/
-
-
-
-
-
-
-};
-
-/*Niveles*/
-
-/*InicioPJ*/
-
-function InicioPJ(){
-
-    var PJ = document.createElement("div");
-    PJ.classList.add("Personaje");  
-    document.querySelector(".mapa").appendChild(PJ);
-
-
-    m.SetCoordsXYMapa(mapa,0,11);
-
 }
-
-/*Salida*/
-
-/*MatarEnemigo*/
-
-/*ComprobarEnemigos*/
 
 /*BarraObjetos*/
 
+function BarraObjetos(){
+
+    var CuadroBarra = document.createElement("div");
+    CuadroBarra.classList.add("CuadroBarra");
+    document.querySelector(".hotbar").appendChild(CuadroBarra);   
+
+    for(let filas=0;filas<3;filas++){
+        var Objeto = document.createElement("div");
+        Objeto.classList.add("Objeto");
+        document.querySelector(".CuadroBarra").appendChild(Objeto);
+    }
+}
+
+/*InicioPJ*/
+
+function PJ(){
+    var X = 0;
+    var Y = 11;
+
+    var PJ = document.createElement("div");
+    PJ.classList.add("Personaje");
+    mapa.appendChild(PJ);
+    mapa2[X][Y] = PJ;
+}
+
+function Enemigo(){
+    for(let i = 0; i < mapa2.length ; i++) {
+        for (let j = 0; j < mapa2[i].length ; j++) {
+            if (mapa2[i][j] == 0){
+             
+            }
+        }
+    }
+}
+
+/*Niveles*/
+/*Salida*/
+/*MatarEnemigo*/
+/*ComprobarEnemigos*/
 /*ObtenerObjeto*/
-
 /*PonerObjetoEnPilar*/
-
-
-
-
-
