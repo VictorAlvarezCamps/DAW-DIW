caad173b580a02434ff4312a922579623efee04b
Victor Alvarez <victor.alvarez.camps@gmail.com>
2019-10-25 13:10:27 +0200

    Cambios pequeños

--- OhMummy/js/main.js
+++ OhMummy/js/main.js
@@ -9,8 +9,7 @@ var PJX = 11;
 var PJY = 0;
 var EnemigoX = Math.floor(Math.random()*21);
 var EnemigoY = Math.floor(Math.random()*10);
-var inventario = document.getElementById("CuadroBarra");
-
+var Item = document.createElement("div");
 var pilares = new Array(15);
 
 var mapa2 = [[2,2,2,2,2,2,2,2,2,2,2,0,2,2,2,2,2,2,2,2,2,2,2],
@@ -29,7 +28,6 @@ var mapa2 = [[2,2,2,2,2,2,2,2,2,2,2,0,2,2,2,2,2,2,2,2,2,2,2],
 var contObjeto1 = 0;
 var contObjeto2 = 0;
 var contObjeto3 = 0;
-
 var RandomPosObjeto = [[2,2],[2,6],[2,10],[2,14],[2,18],
                        [5,2],[5,6],[5,10],[5,14],[5,18],
                        [8,2],[8,6],[8,10],[8,14],[8,18]];
@@ -37,18 +35,20 @@ var RandomPosObjeto = [[2,2],[2,6],[2,10],[2,14],[2,18],
 let RandomNumberObjeto;
 var Objeto = ["Barril","BolaCanon","Canon"];
 var Enemigos = new Array();
-var CuadroBarra = document.createElement("div");
-var Item = document.createElement("div");
+
 
 window.onload = function(){
 
-    /*
-        for(let i=1;i<=20;i++){
+    
+        /*for(let i=1;i<20;i++){
 
             CrearMapa();
-
-            if(Enemigos.length == 1){
-                Enemigos.push(div.classList.add("Enemigo"));
+            for(let i=0;i<mapa2.length;i++){
+                for(let j=0;j<mapa2.length;j++){
+                    if(mapa2[i][j] == 0 && mapa2[i][j].classList.contains("Enemigo")){
+                        Enemigos.push(div.classList.add("Enemigo"));
+                    }
+                }
             }
 
             for(let j=0;j<inventario.length;j++){
@@ -57,20 +57,15 @@ window.onload = function(){
                 }else{
                     setInterval(CerrarPuerta, 1000);
                 }
-            }       
+            }    
             
-        }   
+        }*/
     
-    */
-    PonerObjetoPilar();
-    CrearMapa();
-
-
     BarraObjetos();
+    PonerObjetoPilar();
+    CrearMapa();    
     document.addEventListener('keydown',mover);
-    PonerObjetoPilar();  
     setInterval(MovimientoEnemigo, 300); //300 son milisegundos
-    
 };
 
 function CerrarPuerta(){
@@ -91,6 +86,7 @@ function AbrirPuerta(){
 /*Detectar movimiento personaje*/
 function mover (event) {
 
+     
     switch(event.key){
         case "a":
             if (mapa2[PJY][PJX-1].className.indexOf("camino") >= 0) {
@@ -98,11 +94,9 @@ function mover (event) {
                 mapa2[PJY][PJX].classList.remove("Personaje");
                 /* Algunos cambios para mover al personaje */
                 mapa2[PJY][PJX-1].classList.add("Personaje");
-                if (mapa2[PJY][PJX-1].classList.contains("Pisadas")){
-                    //mapa2[PJY][PJX-1].classList.remove("Pisadas");
-                }
                 PJX--;
             }
+             
         break;
         case "d":
             if (mapa2[PJY][PJX+1].className.indexOf("camino") >= 0) {
@@ -112,6 +106,7 @@ function mover (event) {
                 mapa2[PJY][PJX+1].classList.add("Personaje");
                 PJX++;
             }
+            
         break;
         case "w":
             if (mapa2[PJY-1][PJX].className.indexOf("camino") >= 0) {
@@ -120,7 +115,8 @@ function mover (event) {
                 mapa2[PJY-1][PJX].classList.add("Personaje");
                 /* Algunos cambios para mover al personaje */
                 PJY--;
-            }    
+            }
+            
         break;
         case "s":
             if (mapa2[PJY+1][PJX].className.indexOf("camino") >= 0) {
@@ -129,9 +125,55 @@ function mover (event) {
                 mapa2[PJY+1][PJX].classList.add("Personaje");
                 /* Algunos cambios para mover al personaje */
                 PJY++;
-            }           
+            } 
+                  
         break;
         default: break;
+    }
+
+    if(mapa2[PJY][PJX-1].className.indexOf("pilar") >= 0) {
+        mapa2[PJY][PJX-1].classList.remove("pilar");
+        mapa2[PJY][PJX-1].classList.add("destapado");
+        if(mapa2[PJY][PJX-1].className.indexOf("Barril") >= 0){
+            inventario.classList.add("Barril");
+        }else if(mapa2[PJY][PJX-1].className.indexOf("BolaCanon") >= 0){
+            inventario.classList.add("BolaCanon");
+        }else if(mapa2[PJY][PJX-1].className.indexOf("Canon") >= 0){
+            inventario.classList.add("Canon");
+        }
+    } 
+    if (mapa2[PJY][PJX+1].className.indexOf("pilar") >= 0) {
+        mapa2[PJY][PJX+1].classList.remove("pilar");
+        mapa2[PJY][PJX+1].classList.add("destapado");
+        if(mapa2[PJY][PJX+1].className.indexOf("Barril") >= 0){
+            inventario.classList.add("Barril");
+        }else if(mapa2[PJY][PJX+1].className.indexOf("BolaCanon") >= 0){
+            inventario.classList.add("BolaCanon");
+        }else if(mapa2[PJY][PJX+1].className.indexOf("Canon") >= 0){
+            inventario.classList.add("Canon");
+        }
+    }
+    if (mapa2[PJY-1][PJX].className.indexOf("pilar") >= 0) {
+        mapa2[PJY-1][PJX].classList.remove("pilar");
+        mapa2[PJY-1][PJX].classList.add("destapado");
+        if(mapa2[PJY-1][PJX].className.indexOf("Barril") >= 0){
+            inventario.classList.add("Barril");
+        }else if(mapa2[PJY-1][PJX].className.indexOf("BolaCanon") >= 0){
+            inventario.classList.add("BolaCanon");
+        }else if(mapa2[PJY-1][PJX].className.indexOf("Canon") >= 0){
+            inventario.classList.add("Canon");
+        }
+    }
+    if (mapa2[PJY+1][PJX].className.indexOf("pilar") >= 0) {
+        mapa2[PJY+1][PJX].classList.remove("pilar");
+        mapa2[PJY+1][PJX].classList.add("destapado");
+        if(mapa2[PJY+1][PJX].className.indexOf("Barril") >= 0){
+            inventario.classList.add("Barril");
+        }else if(mapa2[PJY+1][PJX].className.indexOf("BolaCanon") >= 0){
+            inventario.classList.add("BolaCanon");
+        }else if(mapa2[PJY+1][PJX].className.indexOf("Canon") >= 0){
+            inventario.classList.add("Canon");
+        }
     }
 }
 
@@ -168,11 +210,8 @@ function MovimientoEnemigo(){
                 mapa2[EnemigoY][++EnemigoX].classList.add("Enemigo");
             }
         break;
-    }    
-
-
+    }
 }
-
 
 /*Crear mapa*/
 
@@ -194,19 +233,25 @@ function CrearMapa(){
                 div.classList.add("pilar");
                 if (camino && ((i - 2)%3 == 0 || i == 2)) { 
                     div.classList.add(pilares[contador]);
-                    div.dataset.posicion = contador;
+                    div.dataset.posicion = contador;                    
                     div.innerText = i + ":" + contador;
-                    console.log(Objeto[2]);
-                    if(mapa2[i][j].classList.contains){
-                    RellenaPilar(j,i,Objeto[0]);
-                    }
                     contador++;
                     camino = false;
+                }
+                if(div.classList.contains("Barril")){
+                    console.log("Barril --- X: "+j+" Y: "+i);
+                    RellenaPilar(j,i,"Barril");
+                }else if(div.classList.contains("Canon")){
+                    console.log("Cañon --- X: "+j+" Y: "+i);
+                    //RellenaPilar(j,i,"Canon");
+                }else if(div.classList.contains("BolaCanon")){
+                    console.log("BolaCañon --- X: "+j+" Y: "+i);
+                    //RellenaPilar(j,i,"BolaCanon");
                 }
             }
             if((PJY == i) && (PJX == j)) div.classList.add("Personaje");
             if(mapa2[EnemigoY][EnemigoX] == 0 && cont == 0){
-                if((EnemigoX == j) && (EnemigoY == i))Enemigos.push(div.classList.add("Enemigo")),cont = 1;
+                if((EnemigoX == j) && (EnemigoY == i))Enemigos.push(div.classList.add("Enemigo"));
             }else if(mapa2[EnemigoY][EnemigoX] == 1 || mapa2[EnemigoY][EnemigoX] == 2){
                 EnemigoX = Math.floor(Math.random()*21);
                 EnemigoY = Math.floor(Math.random()*10);
@@ -216,53 +261,38 @@ function CrearMapa(){
             mapa2[i][j] = div;
         }
     }
-
 }
-
 
 /*BarraObjetos*/
 
 function BarraObjetos(){
-
-    CuadroBarra.classList.add("CuadroBarra");
-    document.querySelector(".hotbar").appendChild(CuadroBarra);
-    for(let filas=0;filas<3;filas++){        
-        Item.classList.add("Objeto");
+    var Inventario = document.createElement("div");
+    Inventario.classList.add("CuadroBarra");
+    document.querySelector(".hotbar").appendChild(Inventario);
+    let cont3=0;
+    while(cont3<3){
+        Item.classList.add("Objeto");        
         document.querySelector(".CuadroBarra").appendChild(Item);
+        cont3++;
     }
-
 }
-
 
 function RellenaPilar(posX,posY,item){
-    mapa2[posX][posY+1].classList.add(item);
-    mapa2[posX][posY+2].classList.add(item);
+    //mapa2[posX][posY+1].classList.add(item);
+    //mapa2[posX][posY+2].classList.add(item);
     mapa2[posX+1][posY].classList.add(item);
-    mapa2[posX+1][posY+1].classList.add(item);
-    mapa2[posX+1][posY+2].classList.add(item);
-}
-
-function ObjetoAleatorio(){
-    var random = Math.floor(Math.random()*Objeto.length);
-    return Objeto.splice(random,1);
-}
-
-function ComprobarContenidoPilar(){
-
-
+    //mapa2[posX+1][posY+1].classList.add(item);
+    //mapa2[posX+1][posY+2].classList.add(item);
 
 }
 
 function PonerObjetoPilar(){
-
     var i = 0;
-
     while (i < Objeto.length) {
-        let pos = Math.floor(Math.random()*RandomPosObjeto.length);
-        if (pilares[pos] === undefined) {
+        let pos = Math.floor(Math.random()*15);
+        if (pilares[pos] === undefined) {            
             pilares[pos] = Objeto[i];
             i++;
         }
-    }
-    
+    }    
 }
