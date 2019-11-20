aef68c1a03f2337ece9d9c2f31dfbe596c93b0e2
Victor Alvarez <victor.alvarez.camps@gmail.com>
2019-11-20 23:21:11 +0100

    Mejorando diseño,modificando steps y código comentado

--- UD03/Interfaz de Migracion del Heraldo de Miskatonic/js/migracion.js
+++ UD03/Interfaz de Migracion del Heraldo de Miskatonic/js/migracion.js
@@ -13,12 +13,50 @@ FUNCIONES PERDIDAS
 ^(;,;)^
 
 */
+/*Hijos de steps*/
+let step;
+let progress;
+let msg;
+
+function actualizarBarra(e){
+    //var barraVideo = document.getElementById("barraVideo");
+    var progreso = 10;
+    e.value += progreso;
+    //console.log(this);
+}
 
 function startMigration(){
 
     // Fragmentos perdidos
     // ^(;,;)^
 
+    //Seleccionamos todos los hijos de steps.
+    let allSteps = document.querySelector("steps");
+    console.log(allSteps);
+    //Accion por cada step, progress y message.
+    for(let i=0;i<allSteps.length;i++){
+        //Cogemos el step.
+        step = allSteps[i].getElementsByTagName("steplabel");
+        
+        //Cogemos la barra.
+        progress = allSteps[i+1];
+
+        //Cogemos el mensaje de completado.
+        msg = allSteps[i+2];
+
+        //Le añadimos al step la clase estabaEscondido para volver a mostrarlo.
+        step.classList.add("estabaEscondido");
+
+        //Añadimos a la barra la misma clase para mostrarlo de nuevo.
+        progress.classList.add("estabaEscondido");
+        //Evento para ir completando la barra
+        progress.addEventListener("transitionend",actualizarBarra);
+
+        //Cogemos el mensaje y añadimos la clase para mostrarlo de nuevo también.
+        msg.classList.add("estabaEscondido");
+        //Añadimos la clase neon
+        msg.classList.add("finalmsg");
+    }
 }
 
 function init(){
